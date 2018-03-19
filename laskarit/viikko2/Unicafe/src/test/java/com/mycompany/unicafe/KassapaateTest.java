398567aab6fad627095653b489b5de93080eb39d
Tero Tapio <teromarkustapio@gmail.com>
2018-03-19 23:04:55 +0200

    kassan maksukorttitestit lisatty, edullisen hinta korjattu 240->250

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -55,7 +55,7 @@ public class KassapaateTest {
     @Test
     public void rahaSiirtyyKassaanEdullistaOstettaessa() {
         kassa.syoEdullisesti(5_00);
-        assertEquals(1002_40, kassa.kassassaRahaa());
+        assertEquals(1002_50, kassa.kassassaRahaa());
     }
     
     @Test
@@ -92,5 +92,89 @@ public class KassapaateTest {
         assertEquals(1000_00, kassa.kassassaRahaa());
     }
     
+    
+    // KORTTIOSTOTESTIT
+    
+    @Test
+    public void kortiltaVeloitetaanOikeaSummaMaukkaasta() {
+        Maksukortti kortti = new Maksukortti(5_00);
+        kassa.syoMaukkaasti(kortti);
+        assertEquals(1_00, kortti.saldo());
+    }
+    
+    @Test
+    public void kortiltaVeloitetaanOikeaSummaEdullisesta() {
+        Maksukortti kortti = new Maksukortti(5_00);
+        kassa.syoEdullisesti(kortti);
+        assertEquals(2_50, kortti.saldo());
+    }
+    
+    @Test
+    public void kortillaVoiOstaaMaukkaan() {
+        Maksukortti kortti = new Maksukortti(5_00);
+        assertTrue(kassa.syoMaukkaasti(kortti));
+    }
+    
+    @Test
+    public void kortillaVoiOstaaEdullisen() {
+        Maksukortti kortti = new Maksukortti(5_00);
+        assertTrue(kassa.syoEdullisesti(kortti));
+    }
+    
+    @Test
+    public void maukkaanMyyntiseurantaPaivittyyKorttiostolla() {
+        Maksukortti kortti = new Maksukortti(5_00);
+        kassa.syoMaukkaasti(kortti);
+        assertEquals(1, kassa.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void edullisenMyyntiseurantaPaivittyyKorttiostolla() {
+        Maksukortti kortti = new Maksukortti(5_00);
+        kassa.syoEdullisesti(kortti);
+        assertEquals(1, kassa.edullisiaLounaitaMyyty());
+    }
+    
+    @Test
+    public void maukastaEiSaaKunKortillaEiRiitaSaldo() {
+        Maksukortti kortti = new Maksukortti(2_00);
+        assertFalse(kassa.syoMaukkaasti(kortti));
+    }
+    
+    @Test
+    public void edullistaEiSaaKunKortillaEiRiitaSaldo() {
+        Maksukortti kortti = new Maksukortti(2_00);
+        assertFalse(kassa.syoEdullisesti(kortti));
+    }
+    
+    @Test
+    public void maukastaEiVeloitetaKunKortillaEiRiitaSaldo() {
+        Maksukortti kortti = new Maksukortti(2_00);
+        kassa.syoMaukkaasti(kortti);
+        assertEquals(2_00, kortti.saldo());
+    }
+
+    @Test
+    public void edullistaEiVeloitetaKunKortillaEiRiitaSaldo() {
+        Maksukortti kortti = new Maksukortti(2_00);
+        kassa.syoEdullisesti(kortti);
+        assertEquals(2_00, kortti.saldo());
+    }
+    
+    @Test
+    public void maukkaanMyyntiaEiLisataKunKortinSaldoEiRiita() {
+        Maksukortti kortti = new Maksukortti(2_00);
+        kassa.syoMaukkaasti(kortti);
+        assertEquals(0, kassa.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void edullisenMyyntiaEiLisataKunKortinSaldoEiRiita() {
+        Maksukortti kortti = new Maksukortti(2_00);
+        kassa.syoEdullisesti(kortti);
+        assertEquals(0, kassa.edullisiaLounaitaMyyty());
+    }
+    
+    // RAHANLATAUS KASSAN KAUTTA
     
 }
