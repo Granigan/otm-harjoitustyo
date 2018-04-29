0db0e9ed4ffe2e8a14fc076b95a3c1342cf328b7
Tero Tapio <teromarkustapio@gmail.com>
2018-04-29 15:36:27 +0300

    fixed the broken Option references. Adding unlocks-list to constructor broke them in the previous commit.

--- projectpodship/src/main/java/podship/daos/EventDao.java
+++ projectpodship/src/main/java/podship/daos/EventDao.java
@@ -24,9 +24,9 @@ public class EventDao {
 
     // These from DB in final
     private Event captainEvent() {
-        Option pickAIonly = new Option("AI alone", new int[]{0, -2, 2, 0, -2, 0, 0});
-        Option pickCivvy = new Option("Elected civilian captain with AI support", new int[]{0, -1, 1, 0, 0, 0, 0});
-        Option pickMilitary = new Option("Highest ranking military officer, with AI", new int[]{0, -1, 0, 0, 1, 0, 0});
+        Option pickAIonly = new Option("AI alone", new int[]{0, -2, 2, 0, -2, 0, 0}, new ArrayList<Integer>());
+        Option pickCivvy = new Option("Elected civilian captain with AI support", new int[]{0, -1, 1, 0, 0, 0, 0}, new ArrayList<Integer>());
+        Option pickMilitary = new Option("Highest ranking military officer, with AI", new int[]{0, -1, 0, 0, 1, 0, 0}, new ArrayList<Integer>());
         ArrayList<Option> capOptions = new ArrayList<>();
         capOptions.add(pickAIonly);
         capOptions.add(pickCivvy);
@@ -42,13 +42,13 @@ public class EventDao {
         pickPop.setEventText("How should we form the population for the ship?");
         pickPop.addOption(new Option("The Elite: The brightest are pressured to go, "
                 + "the richest buy their way in.",
-                new int[]{0, -2, -1, -1, 1, 0, -1}));
+                new int[]{0, -2, -1, -1, 1, 0, -1}, new ArrayList<Integer>()));
         pickPop.addOption(new Option("The Desperate: Those we can spare, generally "
                 + "under-educated but eager to go.",
-                new int[]{0, 0, 2, 0, 1, 0, 0}));
+                new int[]{0, 0, 2, 0, 1, 0, 0}, new ArrayList<Integer>()));
         pickPop.addOption(new Option("A Cross Section of society: include every "
                 + "level of society in the same scale.",
-                new int[]{0, 0, 0, 0, 1, 0, 0}));
+                new int[]{0, 0, 0, 0, 1, 0, 0}, new ArrayList<Integer>()));
         return pickPop;
     }
 
@@ -57,11 +57,11 @@ public class EventDao {
         Random r = new Random();
         Event debug = new Event();
         debug.setEventText("DEBUG-event");
-        debug.addOption(new Option("first option", new int[]{5, 5, 5, 5, 5, 5, 5}));
-        debug.addOption(new Option("second option", new int[]{-5, -5, -5, -5, -5, -5, -5}));
+        debug.addOption(new Option("first option", new int[]{5, 5, 5, 5, 5, 5, 5}, new ArrayList<Integer>()));
+        debug.addOption(new Option("second option", new int[]{-5, -5, -5, -5, -5, -5, -5}, new ArrayList<Integer>()));
         debug.addOption(new Option("third option",
                 new int[]{r.nextInt(10) - 5, r.nextInt(10) - 5, r.nextInt(10) - 5, r.nextInt(10) - 5,
-                    r.nextInt(10) - 5, r.nextInt(10) - 5, r.nextInt(10) - 5}));
+                    r.nextInt(10) - 5, r.nextInt(10) - 5, r.nextInt(10) - 5}, new ArrayList<Integer>()));
         return debug;
     }
 
